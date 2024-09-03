abf4ed285d7bd2e214355deddfbf67367aacecee
arcFlow-dev <mail.arcflow@gmail.com>
2024-09-03 11:38:25 +0330

    Update(titles function added) scraper.py

--- scraper.py
+++ scraper.py
@@ -1,14 +1,15 @@
 import requests
 
 
-def get_data():
+def get_matches_data():
     data = []
     for day in range(-2, 1):
-        API_URL = f"https://web-api.varzesh3.com/v1.0/livescore/{day}"
-        r = requests.get(API_URL)
+        API_URL_MATCHES = f"https://web-api.varzesh3.com/v1.0/livescore/{day}"
+        r = requests.get(API_URL_MATCHES)
         if r.status_code == 200:
             data += r.json()["matches"]
     return data
+
 
 
 def get_game_result(team1, team2):
@@ -18,3 +19,14 @@ def get_game_result(team1, team2):
             return f'{match["hostName"]} {match["matchGoals"]["host"]} - {match["matchGoals"]["guest"]} {match["guestName"]}'
         elif match["hostName"] == team2 and match["guestName"] == team1:
             return f'{match["hostName"]}{match["matchGoals"]["host"]}-{match["matchGoals"]["guest"]}{match["guestName"]}'
+
+def get_news_title():
+    data = []
+    titles  = []
+    API_URL_NEWS = "https://web-api.varzesh3.com/v1.0/news/most-commented?includeSports[0]=Football&includeSports[1]=Futsal&includeSports[2]=BeachSoccer"
+    request = requests.get(API_URL_NEWS)
+    data += request.json()
+    if request.status_code == 200:
+        for object in data:
+            titles.append(object["title"])
+        return titles[0:5]
